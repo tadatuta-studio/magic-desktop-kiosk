23f52080b06a7a3dd2bd5d7cc6e0cb6534f5b178
Vladimir Grinenko <i@tadatuta.com>
2016-04-07 01:07:45 +0300

    Added temporary stuff for deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+enb make
+add-bom -f desktop.bundles/index/index.html
+cp desktop.bundles/index/index.html /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/MagicDesktop.Website/Views/Landing/Landing3.cshtml
+cp desktop.bundles/index/index.min.css /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/MagicDesktop.Website/content/landing3.css
+cp desktop.bundles/index/index.min.js /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/MagicDesktop.Website/scripts/scripts.js
+

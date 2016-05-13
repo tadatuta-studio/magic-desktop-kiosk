248fe49ec4ad84a81d33f2b6faedbbce77306721
Vladimir Grinenko <i@tadatuta.com>
2016-05-14 01:32:21 +0300

    Update deplay script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 enb make
 add-bom -f desktop.bundles/index/index.html
-cp desktop.bundles/index/index.html /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/MagicDesktop.Website/Views/Landing/Landing3.cshtml
-cp desktop.bundles/index/index.min.css /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/MagicDesktop.Website/content/landing3.css
-cp desktop.bundles/index/index.min.js /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/MagicDesktop.Website/scripts/scripts.js
-
+cp desktop.bundles/index/index.html /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/kiosk.magicdesktop.com/default.html
+cp desktop.bundles/index/index.min.css /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/kiosk.magicdesktop.com/content/styles.css
+cp desktop.bundles/index/index.min.js /Users/tadatuta/projects/studio/ivan/MagicDesktop.Website/kiosk.magicdesktop.com/scripts/scripts.js
